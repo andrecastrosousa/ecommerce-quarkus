b36f9316d9994328ac1fc81708f18192d86458ef
Gonçalo <goncaloscravo@gmail.com>
2023-08-15 19:00:11 +0100

    Add: Implementation of Order service

--- src/main/java/mindswap/academy/order/service/OrderService.java
+++ src/main/java/mindswap/academy/order/service/OrderService.java
@@ -7,9 +7,11 @@ import jakarta.ws.rs.WebApplicationException;
 import mindswap.academy.order.converter.OrderConverter;
 import mindswap.academy.order.dto.OrderCreateDto;
 import mindswap.academy.order.dto.OrderDto;
-import mindswap.academy.model.Order;
+import mindswap.academy.order.dto.OrderUpdatedDto;
+import mindswap.academy.order.model.Order;
 import mindswap.academy.order.repository.OrderRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
@@ -21,7 +23,12 @@ public class OrderService {
     OrderConverter orderConverter;
 
     public List<OrderDto> getAll() {
-        return null;
+       List<Order> orders = orderRepository.findAll().list();
+       List<OrderDto> orderDtos = new ArrayList<>();
+       for(Order o : orders){
+           orderDtos.add(orderConverter.toDto(o));
+       }
+       return orderDtos;
     }
 
     public OrderDto getById( Long orderId) {
@@ -31,8 +38,9 @@ public class OrderService {
     }
 
     public OrderDto create( OrderCreateDto orderCreateDto) {
-
-        return null;
+        Order order = orderConverter.fromOrderCreateDto(orderCreateDto);
+        orderRepository.persist(order);
+        return orderConverter.toDto(order);
     }
 
     public void delete( Long orderId) {
@@ -41,8 +49,21 @@ public class OrderService {
         orderRepository.delete(order);
     }
 
-    public OrderDto update( Long orderId) {
-
-        return null;
+    public OrderDto update(Long orderId, OrderUpdatedDto orderUpdatedDto) {
+        Order existingOrder = orderRepository.findByIdOptional(orderId)
+                .orElseThrow(() -> new WebApplicationException("Order not found", 404));
+        if(!orderUpdatedDto.getOrderItems().equals(existingOrder.getOrderItems())){
+            throw new WebApplicationException("Order items can not be changed", 400);
+        }
+        if(!orderUpdatedDto.getPaymentMethod().equals(existingOrder.getPaymentMethod())){
+            throw new WebApplicationException("Order payment can not be changed", 400);
+        }
+        if(!(orderUpdatedDto.getTotal() == existingOrder.getTotal())) {
+            throw new WebApplicationException("Order price can not be changed", 400);
+        }
+        existingOrder.setOrderDatetime(orderUpdatedDto.getOrderDatetime());
+        existingOrder.setShipping(orderUpdatedDto.getShipping());
+        orderRepository.persist(existingOrder);
+        return orderConverter.toDto(existingOrder);
     }
 }
