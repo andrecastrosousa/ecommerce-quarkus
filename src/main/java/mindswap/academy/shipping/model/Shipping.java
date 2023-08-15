25b784a75b29cd3feecfadb71f3cb2284c845251
André Sousa <andre.castro.sousa@gmail.com>
2023-08-15 14:40:00 +0000

    add: shipping and shipping type entities

--- src/main/java/mindswap/academy/shipping/model/Shipping.java
+++ src/main/java/mindswap/academy/shipping/model/Shipping.java
@@ -1,13 +1,41 @@
 package mindswap.academy.shipping.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import mindswap.academy.address.model.Address;
 
 @Entity
 public class Shipping {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @OneToOne
+    private Address address;
+
+    @ManyToOne
+    private ShippingType type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public ShippingType getType() {
+        return type;
+    }
+
+    public void setType(ShippingType type) {
+        this.type = type;
+    }
 }
--- src/main/java/mindswap/academy/shipping/model/ShippingType.java
+++ src/main/java/mindswap/academy/shipping/model/ShippingType.java
@@ -10,4 +10,32 @@ public class ShippingType {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String type;
+
+    private double cost;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
 }
