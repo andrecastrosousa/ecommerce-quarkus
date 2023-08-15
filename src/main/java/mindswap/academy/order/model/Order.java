4b02f34cde881495c01a17ab18b1bd5762e0e32a
André Sousa <andre.castro.sousa@gmail.com>
2023-08-15 14:37:01 +0000

    add: order and orderItem entities

--- src/main/java/mindswap/academy/order/model/Order.java
+++ src/main/java/mindswap/academy/order/model/Order.java
@@ -1,13 +1,77 @@
 package mindswap.academy.order.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import mindswap.academy.payment.model.PaymentMethod;
+import mindswap.academy.shipping.model.Shipping;
+import mindswap.academy.user.model.User;
+
+import java.util.List;
 
 @Entity
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private double total;
+
+    @OneToMany
+    private List<OrderItem> items;
+
+    @ManyToOne
+    @JsonIgnore
+    private User user;
+
+    private Shipping shipping;
+
+    private PaymentMethod paymentMethod;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
+    public List<OrderItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<OrderItem> items) {
+        this.items = items;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Shipping getShipping() {
+        return shipping;
+    }
+
+    public void setShipping(Shipping shipping) {
+        this.shipping = shipping;
+    }
+
+    public PaymentMethod getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
 }
--- src/main/java/mindswap/academy/order/model/OrderItem.java
+++ src/main/java/mindswap/academy/order/model/OrderItem.java
@@ -1,13 +1,53 @@
 package mindswap.academy.order.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import mindswap.academy.item.model.Item;
 
 @Entity
 public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne
+    @JsonIgnore
+    private Order order;
+
+    @ManyToOne
+    private Item item;
+
+    private int quantity;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public void setItem(Item item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
