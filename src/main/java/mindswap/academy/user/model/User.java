c17f986dd637601f913f8c135418be5bd058986d
André Sousa <andre.castro.sousa@gmail.com>
2023-08-15 14:50:16 +0000

    add: user entity

--- src/main/java/mindswap/academy/user/model/User.java
+++ src/main/java/mindswap/academy/user/model/User.java
@@ -4,10 +4,82 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import mindswap.academy.address.model.Address;
+import mindswap.academy.order.model.Order;
+
+import java.util.List;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
+
+    private int phoneNumber;
+
+    private int nif;
+
+    private String citizenCard;
+
+    private List<Address> addresses;
+
+    private List<Order> orders;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getNif() {
+        return nif;
+    }
+
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
+
+    public String getCitizenCard() {
+        return citizenCard;
+    }
+
+    public void setCitizenCard(String citizenCard) {
+        this.citizenCard = citizenCard;
+    }
+
+    public List<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
