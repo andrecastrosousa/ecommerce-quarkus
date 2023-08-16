81a123ca6137daa06982d08165024154b61c1358
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-08-16 10:02:42 +0100

    add: item dto and converter

--- src/main/java/mindswap/academy/item/converter/ItemConverter.java
+++ src/main/java/mindswap/academy/item/converter/ItemConverter.java
@@ -1,7 +1,24 @@
 package mindswap.academy.item.converter;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import mindswap.academy.item.dto.ItemCreateDto;
+import mindswap.academy.item.dto.ItemDto;
+import mindswap.academy.item.model.Item;
+
+@ApplicationScoped
 public class ItemConverter {
 
+    public Item toEntityFromCreateDto(ItemCreateDto itemCreateDto){
+        return Item.builder()
+                .withName(itemCreateDto.getName())
+                .withPrice(itemCreateDto.getPrice())
+                .withCategory(itemCreateDto.getCategories())
+                .withDescription(itemCreateDto.getDescription())
+                .build();
+    }
+    public ItemDto toDto(Item item){
+        return new ItemDto(item.getName(), item.getPrice());
+    }
 
 
 
--- src/main/java/mindswap/academy/item/dto/ItemDto.java
+++ src/main/java/mindswap/academy/item/dto/ItemDto.java
@@ -1,4 +1,31 @@
 package mindswap.academy.item.dto;
 
 public class ItemDto {
+    private String name;
+
+    private Double price;
+
+    public ItemDto() {
+    }
+
+    public ItemDto(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
