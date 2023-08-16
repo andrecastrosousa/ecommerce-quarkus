49878ab9b5454b8acf978752c82f735890b1b57f
Gonçalo <goncaloscravo@gmail.com>
2023-08-16 18:53:05 +0100

    Add: new tests to order resource

--- src/main/java/mindswap/academy/order/dto/OrderCreateDto.java
+++ src/main/java/mindswap/academy/order/dto/OrderCreateDto.java
@@ -1,11 +1,22 @@
 package mindswap.academy.order.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public class OrderCreateDto {
 
 
+    @NotNull
     private LocalDateTime orderDatetime;
+
+    public OrderCreateDto() {
+    }
+
+    public OrderCreateDto(LocalDateTime orderDatetime) {
+        this.orderDatetime = orderDatetime;
+    }
 
     public LocalDateTime getOrderDatetime() {
         return orderDatetime;
