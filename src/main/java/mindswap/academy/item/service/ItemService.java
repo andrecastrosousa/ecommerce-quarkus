4cb836d1982d36038c3f9314d5aef9c65da149a0
Gabriel Saramago <gabrielsaramago@gmail.com>
2023-08-16 10:56:04 +0100

    add: item service

--- src/main/java/mindswap/academy/item/service/ItemService.java
+++ src/main/java/mindswap/academy/item/service/ItemService.java
@@ -1,4 +1,18 @@
 package mindswap.academy.item.service;
 
-public class ItemService {
+import mindswap.academy.item.dto.ItemCreateDto;
+import mindswap.academy.item.dto.ItemDto;
+import mindswap.academy.item.model.Item;
+
+import java.util.List;
+
+public interface ItemService {
+    List<Item> getAll();
+    Item getByName(String name);
+    Item getById(Long id);
+    ItemDto create(ItemCreateDto itemCreateDto);
+    List<ItemDto> createBulk(List<ItemCreateDto> items);
+    Item updateById(Long id, Item item);
+    void deleteById(Long id);
+
 }
