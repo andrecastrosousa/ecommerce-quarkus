53560dbdbe9759039f60a577a3b793b976b104b9
André Sousa <andre.castro.sousa@gmail.com>
2023-08-16 17:11:16 +0000

    add: test to register endpoint

--- src/main/java/mindswap/academy/authentication/dto/AuthRegisterDto.java
+++ src/main/java/mindswap/academy/authentication/dto/AuthRegisterDto.java
@@ -6,6 +6,13 @@ public class AuthRegisterDto {
     private String firstName;
     private String lastName;
 
+    public AuthRegisterDto(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public String getEmail() {
         return email;
     }
