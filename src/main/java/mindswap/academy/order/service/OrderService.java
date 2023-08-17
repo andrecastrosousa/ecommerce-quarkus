cf1f81db2d160d67455a8865d1b75b0611225ac5
Gonçalo <goncaloscravo@gmail.com>
2023-08-17 13:06:00 +0100

    Add: Order service interface

--- src/main/java/mindswap/academy/order/service/OrderService.java
+++ src/main/java/mindswap/academy/order/service/OrderService.java
@@ -1,62 +1,19 @@
 package mindswap.academy.order.service;
 
-
-import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
-import jakarta.ws.rs.WebApplicationException;
-import mindswap.academy.order.converter.OrderConverter;
 import mindswap.academy.order.dto.OrderCreateDto;
 import mindswap.academy.order.dto.OrderDto;
 import mindswap.academy.order.dto.OrderUpdatedDto;
-import mindswap.academy.order.model.Order;
-import mindswap.academy.order.repository.OrderRepository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@ApplicationScoped
-public class OrderService {
+public interface OrderService {
+    List<OrderDto> getAll();
 
-    @Inject
-    OrderRepository orderRepository;
-    @Inject
-    OrderConverter orderConverter;
+    OrderDto getById(Long orderId);
 
-    public List<OrderDto> getAll() {
-       List<Order> orders = orderRepository.findAll().list();
-       List<OrderDto> orderDtos = new ArrayList<>();
-       for(Order o : orders){
-           orderDtos.add(orderConverter.toDto(o));
-       }
-       return orderDtos;
-    }
+    OrderDto create(OrderCreateDto orderCreateDto);
 
-    public OrderDto getById(Long orderId) {
-        Order order = orderRepository.findByIdOptional(orderId)
-                .orElseThrow(() -> new WebApplicationException("Order not Found", 404));
-        return orderConverter.toDto(order);
-    }
+    void delete(Long orderId);
 
-    public OrderDto create(OrderCreateDto orderCreateDto) {
-        Order order = orderConverter.fromOrderCreateDto(orderCreateDto);
-        orderRepository.persist(order);
-        return orderConverter.toDto(order);
-    }
-
-    public void delete(Long orderId) {
-        Order order = orderRepository.findByIdOptional(orderId)
-                .orElseThrow(() -> new WebApplicationException("Order not Found", 404));
-        orderRepository.delete(order);
-    }
-
-    public OrderDto update(Long orderId, OrderUpdatedDto orderUpdatedDto) {
-        Order existingOrder = orderRepository.findByIdOptional(orderId)
-                .orElseThrow(() -> new WebApplicationException("Order not found", 404));
-        if(!orderUpdatedDto.getId().equals(existingOrder.getId())){
-            throw new WebApplicationException("Id can not be changed", 400);
-        }
-        existingOrder.setOrderDatetime(orderUpdatedDto.getOrderDatetime());
-        orderRepository.persist(existingOrder);
-        return orderConverter.toDto(existingOrder);
-    }
+    OrderDto update(Long orderId, OrderUpdatedDto orderUpdatedDto);
 }
---
+++ src/main/java/mindswap/academy/order/service/OrderServiceImp.java
@@ -0,0 +1,67 @@
+package mindswap.academy.order.service;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.WebApplicationException;
+import mindswap.academy.order.converter.OrderConverter;
+import mindswap.academy.order.dto.OrderCreateDto;
+import mindswap.academy.order.dto.OrderDto;
+import mindswap.academy.order.dto.OrderUpdatedDto;
+import mindswap.academy.order.model.Order;
+import mindswap.academy.order.repository.OrderRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@ApplicationScoped
+public class OrderServiceImp implements OrderService {
+
+    @Inject
+    OrderRepository orderRepository;
+    @Inject
+    OrderConverter orderConverter;
+
+    @Override
+    public List<OrderDto> getAll() {
+       List<Order> orders = orderRepository.findAll().list();
+       List<OrderDto> orderDtos = new ArrayList<>();
+       for(Order o : orders){
+           orderDtos.add(orderConverter.toDto(o));
+       }
+       return orderDtos;
+    }
+
+    @Override
+    public OrderDto getById(Long orderId) {
+        Order order = orderRepository.findByIdOptional(orderId)
+                .orElseThrow(() -> new WebApplicationException("Order not Found", 404));
+        return orderConverter.toDto(order);
+    }
+
+    @Override
+    public OrderDto create(OrderCreateDto orderCreateDto) {
+        Order order = orderConverter.fromOrderCreateDto(orderCreateDto);
+        orderRepository.persist(order);
+        return orderConverter.toDto(order);
+    }
+
+    @Override
+    public void delete(Long orderId) {
+        Order order = orderRepository.findByIdOptional(orderId)
+                .orElseThrow(() -> new WebApplicationException("Order not Found", 404));
+        orderRepository.delete(order);
+    }
+
+    @Override
+    public OrderDto update(Long orderId, OrderUpdatedDto orderUpdatedDto) {
+        Order existingOrder = orderRepository.findByIdOptional(orderId)
+                .orElseThrow(() -> new WebApplicationException("Order not found", 404));
+        if(!orderUpdatedDto.getId().equals(existingOrder.getId())){
+            throw new WebApplicationException("Id can not be changed", 400);
+        }
+        existingOrder.setOrderDatetime(orderUpdatedDto.getOrderDatetime());
+        orderRepository.persist(existingOrder);
+        return orderConverter.toDto(existingOrder);
+    }
+}
