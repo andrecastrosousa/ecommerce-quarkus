446f67bb902451cb0b21f293086503e1be835a3d
André Sousa <andre.castro.sousa@gmail.com>
2023-08-17 18:54:21 +0000

    fix: register endpoint

--- src/main/java/mindswap/academy/authentication/repository/KeycloakRepositoryImpl.java
+++ src/main/java/mindswap/academy/authentication/repository/KeycloakRepositoryImpl.java
@@ -13,12 +13,13 @@ import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.admin.client.resource.UsersResource;
 import org.keycloak.representations.idm.CredentialRepresentation;
+import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
 
 @ApplicationScoped
 public class KeycloakRepositoryImpl implements KeycloakRepository {
-    @ConfigProperty(name = "keycloak.realm")
-    private String realm;
+    @ConfigProperty(name = "keycloak.baseRealm")
+    private String baseRealm;
 
     @ConfigProperty(name = "keycloak.clientId")
     private String clientId;
@@ -32,8 +33,11 @@ public class KeycloakRepositoryImpl implements KeycloakRepository {
     @ConfigProperty(name = "keycloak.password")
     private String password;
 
-    private String serverUrl = "http://localhost:8081/";
+    @ConfigProperty(name = "keycloak.server-url")
+    private String serverUrl;
 
+    @ConfigProperty(name = "keycloak.innerRealm")
+    private String innerRealm;
 
     @Inject
     private Keycloak keycloak;
@@ -42,7 +46,7 @@ public class KeycloakRepositoryImpl implements KeycloakRepository {
     public void initKeycloak() {
         keycloak = KeycloakBuilder.builder()
                 .serverUrl(serverUrl)
-                .realm(realm)
+                .realm(baseRealm)
                 .clientId(clientId)
                 .grantType(grantType)
                 .username(username)
@@ -57,7 +61,7 @@ public class KeycloakRepositoryImpl implements KeycloakRepository {
 
     @Override
     public String createAuthentication(UserRepresentation userRepresentation, String password) {
-        UsersResource usersResource = keycloak.realm("quarkus").users();
+        UsersResource usersResource = keycloak.realm(innerRealm).users();
 
         userRepresentation.setEnabled(true);
 
@@ -69,7 +73,7 @@ public class KeycloakRepositoryImpl implements KeycloakRepository {
         passwordCred.setType(CredentialRepresentation.PASSWORD);
         passwordCred.setValue(password);
 
-        UserResource userResource = keycloak.realm(realm).users().get(userId);
+        UserResource userResource = keycloak.realm(innerRealm).users().get(userId);
 
         userResource.resetPassword(passwordCred);
 
@@ -80,15 +84,14 @@ public class KeycloakRepositoryImpl implements KeycloakRepository {
     public Auth login(Auth auth) {
         Keycloak keycloak1 = KeycloakBuilder.builder()
                 .serverUrl(serverUrl)
-                .realm("quarkus")
+                .realm(baseRealm)
                 .clientId(clientId)
                 .grantType(grantType)
-                .scope("openid")
                 .username(auth.getEmail())
                 .password(auth.getPassword())
                 .build();
 
-        auth.setPassword(keycloak1.tokenManager().getAccessToken().getToken());
+        auth.setToken(keycloak1.tokenManager().getAccessToken().getToken());
 
         return auth;
     }
