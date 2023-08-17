905d9d40f1d8518c114d9ae43bf4dfea38e917aa
André Sousa <andre.castro.sousa@gmail.com>
2023-08-17 18:54:21 +0000

    fix: register endpoint

--- src/main/java/mindswap/academy/authentication/resource/AuthenticationResource.java
+++ src/main/java/mindswap/academy/authentication/resource/AuthenticationResource.java
@@ -1,21 +1,29 @@
 package mindswap.academy.authentication.resource;
 
+import io.quarkus.security.identity.SecurityIdentity;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.SecurityContext;
 import mindswap.academy.authentication.dto.AuthDto;
 import mindswap.academy.authentication.dto.AuthLoginDto;
 import mindswap.academy.authentication.dto.AuthRegisterDto;
 import mindswap.academy.authentication.service.AuthenticationService;
+
+import java.security.Principal;
 
 @Path("")
 public class AuthenticationResource {
 
     @Inject
     AuthenticationService authenticationService;
+
+    @Inject
+    SecurityIdentity identity;
 
     @POST
     @Path("/login")
